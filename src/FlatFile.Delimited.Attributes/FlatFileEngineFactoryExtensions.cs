4d4fca09e95d39c8ec0965dc373ecc55e0cf05cd
Pavel Nosovich <forcewake@gmail.com>
2015-01-08 15:16:17 +0300

    Add GetEngine for Delimited type

---
+++ src/FlatFile.Delimited.Attributes/FlatFileEngineFactoryExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace FlatFile.Delimited.Attributes
+{
+    using System;
+    using FlatFile.Core;
+    using FlatFile.Delimited;
+    using FlatFile.Delimited.Attributes.Infrastructure;
+
+    public static class FlatFileEngineFactoryExtensions
+    {
+        public static IFlatFileEngine<T> GetEngine<T>(
+            this IFlatFileEngineFactory<IDelimitedFieldSettingsContainer> engineFactory,
+            Func<string, Exception, bool> handleEntryReadError = null)
+            where T : class, new()
+        {
+            var descriptorProvider = new DelimitedLayoutDescriptorProvider();
+
+            var descriptor = descriptorProvider.GetDescriptor<T>();
+
+            return engineFactory.GetEngine<T>(descriptor, handleEntryReadError);
+        }
+    }
+}
