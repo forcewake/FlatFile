ba4ac7501560d413b5ecacd1020cef46c786feb6
Pavel Nosovich <forcewake@gmail.com>
2015-01-09 16:44:32 +0300

    Add benchmark for reading

--- src/FlatFile.Benchmark/FlatFileVsCsvHelperBenchmark.cs
+++ src/FlatFile.Benchmark/FlatFileVsCsvHelperBenchmark.cs
@@ -3,11 +3,14 @@
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
+    using System.Text;
     using BenchmarkIt;
     using CsvHelper;
     using FlatFile.Benchmark.Entities;
     using FlatFile.Benchmark.Mapping;
     using FlatFile.Delimited.Implementation;
+    using FluentAssertions;
     using Xunit;
 
     public class FlatFileVsCsvHelperBenchmark
@@ -54,6 +57,48 @@
                         var flatFile = factory.GetEngine<CustomObject>(layout);
 
                         flatFile.Write(stream, records);
+                    }
+                })
+                .WithWarmup(1000)
+                .For(10000)
+                .Iterations()
+                .PrintComparison();
+        }
+
+        [Fact(Skip = "Bug for GUID")]
+        public void ReadAllRecordsWithMapping()
+        {
+            const string fileContent =
+@"String Column,Int Column,Guid Column
+one,1,f96a1c66-4777-4642-86fa-703098065f5f
+two,2,06776ed9-d33f-470f-bd3f-8db842356330
+";
+            Benchmark.This("CsvWriter.WriteRecords", () =>
+            {
+                using (var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(fileContent)))
+                using (var streamReader = new StreamReader(memoryStream))
+                using (var reader = new CsvReader(streamReader))
+                {
+                    reader.Configuration.RegisterClassMap<CsvHelperMappingForCustomObject>();
+
+                    var objects = reader.GetRecords<CustomObject>().ToArray();
+
+                    objects.Length.Should().Be(2);
+                }
+            })
+                .Against.This("FlatFileEngine.Write", () =>
+                {
+                    var layout = new FlatFileMappingForCustomObject();
+                    using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(fileContent)))
+                    {
+                        var factory = new DelimitedFileEngineFactory();
+
+                        var flatFile = factory.GetEngine<CustomObject>(layout);
+
+                        var objects = flatFile.Read(stream).ToArray();
+
+                        objects.Length.Should().Be(2);
+
                     }
                 })
                 .WithWarmup(1000)
