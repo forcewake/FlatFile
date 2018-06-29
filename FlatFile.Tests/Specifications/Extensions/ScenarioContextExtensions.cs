0d191aaab4c87ef599f258f48e4d35ce5725c49f
Peter T. LaComb Jr <peter@lacombfamily.com>
2018-06-29 06:49:12 -0400

    SpecFLow doesn't work at the moment.

--- FlatFile.Tests/Specifications/Extensions/ScenarioContextExtensions.cs
+++ FlatFile.Tests/Specifications/Extensions/ScenarioContextExtensions.cs
@@ -1,22 +1,22 @@
-namespace FlatFile.Tests.Specifications.Extensions
-{
-    using System;
-    using System.Linq.Expressions;
-    using FlatFile.Core.Extensions;
-    using TechTalk.SpecFlow;
+//namespace FlatFile.Tests.Specifications.Extensions
+//{
+//    using System;
+//    using System.Linq.Expressions;
+//    using FlatFile.Core.Extensions;
+//    using TechTalk.SpecFlow;
 
-    public static class ScenarioContextExtensions
-    {
-        public static void Add<T>(this ScenarioContext context, Expression<Func<T>> expression, object value)
-        {
-            var name = expression.GetMemberName();
-            context.Add(name, value);
-        }
+//    public static class ScenarioContextExtensions
+//    {
+//        public static void Add<T>(this ScenarioContext context, Expression<Func<T>> expression, object value)
+//        {
+//            var name = expression.GetMemberName();
+//            context.Add(name, value);
+//        }
 
-        public static bool TryGetValue<T>(this ScenarioContext context, Expression<Func<T>> expression, out T value)
-        {
-            var name = expression.GetMemberName();
-            return context.TryGetValue(name, out value);
-        }
-    }
-}
+//        public static bool TryGetValue<T>(this ScenarioContext context, Expression<Func<T>> expression, out T value)
+//        {
+//            var name = expression.GetMemberName();
+//            return context.TryGetValue(name, out value);
+//        }
+//    }
+//}
