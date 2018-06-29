19e30723d4cd9197a43347aa6ebb9a5b9058585e
Peter T. LaComb Jr <peter@lacombfamily.com>
2018-06-28 22:22:02 -0400

    Almost builds in VSCode

---
+++ FlatFile.Benchmark/Converters/CsvHelperTypeConverterForCustomType.cs
@@ -0,0 +1,26 @@
+﻿namespace FlatFile.Benchmark.Converters
+{
+    using CsvHelper;
+    using CsvHelper.Configuration;
+    using CsvHelper.TypeConversion;
+
+    public class CsvHelperTypeConverterForCustomType : ITypeConverter
+    {
+        private readonly FlatFileTypeConverterForCustomType converter;
+
+        public CsvHelperTypeConverterForCustomType()
+        {
+            converter = new FlatFileTypeConverterForCustomType();
+        }
+
+        public string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
+        {
+            return converter.ConvertToString(value);
+        }
+
+        public object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            return converter.ConvertFromString(text);
+        }
+    }
+}
---
+++ FlatFile.Benchmark/Mapping/CsvHelperMappingForCustomObject.cs
@@ -0,0 +1,17 @@
+﻿namespace FlatFile.Benchmark.Mapping
+{
+    using CsvHelper.Configuration;
+    using FlatFile.Benchmark.Converters;
+    using FlatFile.Benchmark.Entities;
+
+    public sealed class CsvHelperMappingForCustomObject : ClassMap<CustomObject>
+    {
+        public CsvHelperMappingForCustomObject()
+        {
+            Map(m => m.CustomTypeColumn).Name("Custom Type Column").Index(3).TypeConverter<CsvHelperTypeConverterForCustomType>();
+            Map(m => m.GuidColumn).Name("Guid Column").Index(2);
+            Map(m => m.IntColumn).Name("Int Column").Index(1);
+            Map(m => m.StringColumn).Name("String Column").Index(0);
+        }
+    }
+}
