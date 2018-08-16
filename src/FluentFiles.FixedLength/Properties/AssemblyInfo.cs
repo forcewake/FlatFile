a0da87c02fb3a5c60a0fe00f51208b1099cd2225
Matthew Hamilton <matthamilton@live.com>
2018-08-16 00:33:36 -0500

    Fix InternalsVisibleTo attributes.

--- src/FluentFiles.FixedLength/Properties/AssemblyInfo.cs
+++ src/FluentFiles.FixedLength/Properties/AssemblyInfo.cs
@@ -37,4 +37,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("FlatFile.Tests")]
+[assembly: InternalsVisibleTo("FluentFiles.Tests")]
