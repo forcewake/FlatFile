571b0b82b3d7111215d48ac498354b9942bb1c0f
Matthew Hamilton <matthamilton@live.com>
2018-08-20 03:37:48 -0500

    Add a couple more test lines.

--- src/FluentFiles.Benchmark/FluentFilesVsCsvHelperRead.cs
+++ src/FluentFiles.Benchmark/FluentFilesVsCsvHelperRead.cs
@@ -15,6 +15,8 @@ namespace FluentFiles.Benchmark
 @"String Column,Int Column,Guid Column,Custom Type Column
 one,1,f96a1c66-4777-4642-86fa-703098065f5f,1|2|3
 two,2,06776ed9-d33f-470f-bd3f-8db842356330,4|5|6
+three,3,ed3085c8-354f-4068-bd73-499b73748189,7|8|9
+four,4,951ca0e2-3cbd-49bf-a1bb-cd2591869cb7,10|11|12
 ";
 
         [Benchmark(Baseline = true)]
