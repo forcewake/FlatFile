8bef984f41788d11a2d434b2dc9ac7c2dfd55e3a
Matthew Hamilton <matthamilton@live.com>
2018-10-06 17:44:07 -0500

    Cache line builders by layout target type.

--- src/FluentFiles.FixedLength/Implementation/FixedLengthLineBuilderFactory.cs
+++ src/FluentFiles.FixedLength/Implementation/FixedLengthLineBuilderFactory.cs
@@ -1,10 +1,21 @@
+using System;
+using System.Collections.Generic;
+
 namespace FluentFiles.FixedLength.Implementation
 {
     public class FixedLengthLineBuilderFactory : IFixedLengthLineBuilderFactory
     {
+        private readonly IDictionary<Type, IFixedLengthLineBuilder> _lineBuilders = new Dictionary<Type, IFixedLengthLineBuilder>();
+
         public IFixedLengthLineBuilder GetBuilder(IFixedLengthLayoutDescriptor descriptor)
         {
-            return new FixedLengthLineBuilder(descriptor);
+            if (!_lineBuilders.TryGetValue(descriptor.TargetType, out var builder))
+            {
+                builder = new FixedLengthLineBuilder(descriptor);
+                _lineBuilders[descriptor.TargetType] = builder;
+            }
+
+            return builder;
         }
     }
 }
