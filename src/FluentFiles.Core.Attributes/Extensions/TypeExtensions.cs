de0d84782f5f8c94c5287452df919a2bd78f6ad4
Matthew Hamilton <matthamilton@live.com>
2019-02-14 01:42:44 -0600

    Minor refactor.

--- src/FluentFiles.Core.Attributes/Extensions/TypeExtensions.cs
+++ src/FluentFiles.Core.Attributes/Extensions/TypeExtensions.cs
@@ -7,14 +7,10 @@
 
     internal static class TypeExtensions
     {
-        public static IEnumerable<PropertyDescription> GetTypeDescription<TAttribute>(this Type targetType) where TAttribute : Attribute
-        {
-            var properties = from p in targetType.GetProperties()
-                where Attribute.IsDefined(p, typeof(TAttribute))
-                let attr = p.GetCustomAttributes(typeof(TAttribute), true)
-                select new PropertyDescription { Property = p, Attributes = attr.Cast<Attribute>().ToArray() };
-
-            return properties;
-        }
+        public static IEnumerable<PropertyDescription> GetTypeDescription<TAttribute>(this Type targetType) where TAttribute : Attribute =>
+            from property in targetType.GetProperties()
+            where Attribute.IsDefined(property, typeof(TAttribute))
+            let attr = property.GetCustomAttributes(typeof(TAttribute), true)
+            select new PropertyDescription(property, attr.Cast<Attribute>().ToArray());
     }
 }
