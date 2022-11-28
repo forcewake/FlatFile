0161ac272120233a360dccc922db37379c126508
ushmut10\MumlDav <DMumladze@hastingsmutual.com>
2022-11-28 13:30:48 -0500

    Signed assemlies with SN key

--- src/FlatFile.Delimited/Properties/AssemblyInfo.cs
+++ src/FlatFile.Delimited/Properties/AssemblyInfo.cs
@@ -34,4 +34,9 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("FlatFile.Tests")]
+[assembly: InternalsVisibleTo("FlatFile.Tests, PublicKey=" +
+    "0024000004800000940000000602000000240000525341310004000001000100d5e09aeb6df493" +
+    "2248127d477e1eec90c618bf391c6327eacd6397154b7172bd5e1e0327b33258fe14a55c8fe0ba" +
+    "5378058bd44809093f0ca0081f11ce14d0ef422681ce1cb66c184e92c907c9c08274ad1d6b9e29" +
+    "8e24be1f84648e688c126673d98c7686facbd392e447165d56632a1b5ffc0d92b1fb19da4f4091" +
+    "5161e2c9")]
